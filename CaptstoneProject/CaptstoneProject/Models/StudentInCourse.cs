4ba3036e2b9b26956196b781b4727ce063d0dc2f
john <john@DANHDCSE61904>
2017-09-24 05:25:32 +0700

    +Create Student Controller, View +Load, search datatable server side +Update Json

---
+++ CaptstoneProject/CaptstoneProject/Models/StudentInCourse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using DataService.Model;
+
+namespace CaptstoneProject.Models
+{
+    public class StudentInCourse
+    {
+        public int ID { get; set; }
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public int CourseID { get; set; }
+        public int SubjectID { get; set; }
+        public int Average { get; set; }
+        
+
+    }
+    public class MarkDBContext : DB_Finance_AcademicEntities
+    {
+        public DbSet<StudentInCourse> StudentsInCourse { get; set; }
+    }
+}
