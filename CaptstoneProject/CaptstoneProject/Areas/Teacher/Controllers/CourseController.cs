d996bf2d07e0f16b3f7096ee02a8cf0a0d81ab14
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2017-09-24 19:32:54 +0700

    Add course detail view

--- CaptstoneProject/CaptstoneProject/Areas/Teacher/Controllers/CourseController.cs
+++ CaptstoneProject/CaptstoneProject/Areas/Teacher/Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,17 +26,76 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
 
                 courses = context.Teachers.Where(q => q.LoginName == loginName).FirstOrDefault()
                     .Courses.Where(q => q.StartDate >= startDate && q.EndDate <= endDate)
-                    .Select(q => new CourseRecordViewModel {
+                    .Select(q => new CourseRecordViewModel
+                    {
                         CourseId = q.Id,
                         Name = q.Subject.SubjectName,
                         Code = q.Subject.SubjectCode,
                         Class = q.ClassName,
                         StartDate = q.StartDate.Value,
                         EndDate = q.EndDate.Value
-                }).ToList();
+                    }).ToList();
             }
 
             return View(courses);
+        }
+
+        public ActionResult CourseDetails(int courseId)
+        {
+            ViewBag.CourseId = courseId;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GetCourseDetails(int courseId)
+        {
+            try
+            {
+                var loginName = (string)Session["loginName"];
+
+                using (var context = new DB_Finance_AcademicEntities())
+                {
+                    var course = await context.Courses.FindAsync(courseId);
+
+                    if (course != null && course.Teacher.LoginName == loginName)
+                    {
+                        ViewBag.ClassName = course.ClassName;
+
+                        var data = course.StudentInCourses.Select(q => new StudentInCourseViewModel
+                        {
+                            UserName = q.Student.LoginName,
+                            StudentCode = q.Student.StudentCode,
+                            Average = q.Average.Value,
+                            MarksComponent = q.StudentCourseMarks.ToList(),
+                            Status = q.Status
+                        });
+
+                        var datatest = course.StudentInCourses.Select(q => new IConvertible[] {
+                            q.Student.StudentCode,
+                            q.Student.LoginName,
+                            //q.StudentCourseMarks.Select(s => s.Mark),
+                            q.Average,
+                            q.Status
+                        });
+
+                        var columns = new List<string[]>();
+                        foreach(var component in data.ElementAt(0).MarksComponent)
+                        {
+                            columns.Add(new string[] { "title", component.CourseMark.ComponentName });
+                        }
+
+                        return Json(new { success = true, columns = columns, data = datatest });
+                    }
+                    else
+                    {
+                        return Json(new { success = false, message = "Access denied." });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, message = e.Message });
+            }
         }
     }
 
@@ -47,6 +107,14 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
         public string Class { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+    }
 
+    public class StudentInCourseViewModel
+    {
+        public string UserName { get; set; }
+        public string StudentCode { get; set; }
+        public double Average { get; set; }
+        public List<StudentCourseMark> MarksComponent { get; set; }
+        public string Status { get; set; }
     }
 }
