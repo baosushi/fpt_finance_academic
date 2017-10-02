1e2ea26c32599a99b3a04b79af24648fb4aada51
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2017-10-03 01:01:43 +0700

    import excel, download excel template for teacher

--- CaptstoneProject/CaptstoneProject/Areas/Teacher/Controllers/CourseController.cs
+++ CaptstoneProject/CaptstoneProject/Areas/Teacher/Controllers/CourseController.cs
@@ -1,7 +1,10 @@
 ﻿using DataService.Model;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -49,12 +52,6 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
             return View(courses);
         }
 
-        //public ActionResult CourseDetails(int courseId)
-        //{
-        //    ViewBag.CourseId = courseId;
-        //    return View();
-        //}
-        
         public ActionResult CourseDetails(int courseId, int semesterId)
         {
             ViewBag.CourseId = courseId;
@@ -89,12 +86,14 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
 
                         var columns = data.ElementAt(0).MarksComponent.Select(q => q.CourseMark.ComponentName).ToList();
                         var semester = semesterId == -1 ? context.Semesters.OrderByDescending(q => q.Year).ThenByDescending(q => q.SemesterInYear).FirstOrDefault() : context.Semesters.Find(semesterId);
-                        var model = new CourseDetailsViewModel {
+                        var model = new CourseDetailsViewModel
+                        {
+                            CourseId = courseId,
                             ComponentNames = columns,
                             StudentInCourse = data,
-                            Semester = semester.Title + " "+semester.Year,
+                            Semester = semester.Title + " " + semester.Year,
                             SubCode = course.Subject.SubjectCode,
-                            SubName= course.Subject.SubjectName,
+                            SubName = course.Subject.SubjectName,
                         };
 
                         //return Json(new { success = true, columns = columns, data = data });
@@ -110,6 +109,188 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
             {
                 return Json(new { success = false, message = e.Message });
             }
+        }
+
+        [HttpPost]
+        public ActionResult DownloadTemplate(int courseId)
+        {
+            MemoryStream ms = new MemoryStream();
+
+            using (var context = new DB_Finance_AcademicEntities())
+            {
+                var course = context.Courses.Find(courseId);
+                var fileName = course.ClassName + " - " + course.CourseName;
+
+                using (ExcelPackage package = new ExcelPackage(ms))
+                {
+                    #region Excel format
+                    ExcelWorksheet ws = package.Workbook.Worksheets.Add("Sheet1");
+                    char StartHeaderChar = 'A';
+                    int StartHeaderNumber = 1;
+                    #region Headers
+                    ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = "No";
+                    ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = "Student ID";
+                    ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = "Student login";
+                    ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = "Name";
+
+                    foreach (var component in course.CourseMarks)
+                    {
+                        ws.Cells["" + (StartHeaderChar) + (StartHeaderNumber + 1)].Value = component.Percentage / 100;
+                        ws.Cells["" + (StartHeaderChar) + (StartHeaderNumber + 1)].Style.Numberformat.Format = "0%";
+                        ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = component.ComponentName;
+                    }
+
+                    var EndHeaderChar = --StartHeaderChar;
+                    var EndHeaderNumber = StartHeaderNumber;
+                    StartHeaderChar = 'A';
+                    StartHeaderNumber = 1;
+                    #endregion
+                    #region Header styling
+                    ws.Cells["" + StartHeaderChar + StartHeaderNumber.ToString() +
+                    ":" + EndHeaderChar + EndHeaderNumber.ToString()].Style.Font.Bold = true;
+                    //ws.Cells["" + StartHeaderChar + StartHeaderNumber.ToString() +
+                    //    ":" + EndHeaderChar + EndHeaderNumber.ToString()].AutoFitColumns();
+                    //ws.Cells["" + StartHeaderChar + StartHeaderNumber.ToString() +
+                    //    ":" + EndHeaderChar + EndHeaderNumber.ToString()]
+                    //    .Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    //ws.Cells["" + StartHeaderChar + StartHeaderNumber.ToString() +
+                    //    ":" + EndHeaderChar + EndHeaderNumber.ToString()]
+                    //    .Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.GreenYellow);
+                    //ws.View.FreezePanes(2, 1);
+
+                    StartHeaderNumber++;
+                    #endregion
+                    #region Set values for available fields
+                    var count = 1;
+                    foreach (var student in course.StudentInCourses)
+                    {
+                        ws.Cells["" + (StartHeaderChar++) + (++StartHeaderNumber)].Value = count++;
+                        ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = student.Student.StudentCode;
+                        ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = student.Student.LoginName;
+                        ws.Cells["" + (StartHeaderChar) + (StartHeaderNumber)].Value = student.Student.LoginName;
+                        //foreach(var mark in student.StudentCourseMarks)
+                        //{
+                        //    ws.Cells["" + (StartHeaderChar++) + (StartHeaderNumber)].Value = mark.Mark.HasValue ? mark.Mark.Value : -1;
+                        //}
+                        StartHeaderChar = 'A';
+                    }
+                    fileName += ".xlsx";
+
+                    StartHeaderNumber = 1;
+                    ws.Cells.AutoFitColumns();
+                    //ws.Cells["" + StartHeaderChar + StartHeaderNumber.ToString() +
+                    //":" + EndHeaderChar + EndHeaderNumber.ToString()].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                    #endregion
+
+                    #endregion
+
+                    package.SaveAs(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    return this.File(ms, contentType, fileName);
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UploadExcel(int courseId)
+        {
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    using (var context = new DB_Finance_AcademicEntities())
+                    {
+                        foreach (string file in Request.Files)
+                        {
+                            var fileContent = Request.Files[file];
+
+                            var course = context.Courses.Find(courseId);
+
+                            var subjectCode = course.Subject.SubjectCode;
+                            var className = course.ClassName;
+                            if (fileContent != null && fileContent.ContentLength > 0)
+                            {
+                                var stream = fileContent.InputStream;
+
+                                using (ExcelPackage package = new ExcelPackage(stream))
+                                {
+                                    var ws = package.Workbook.Worksheets.First();
+                                    var totalCol = ws.Dimension.Columns;
+                                    var totalRow = ws.Dimension.Rows;
+                                    var studentCodeCol = 2;
+                                    var titleRow = 1;
+                                    var firstRecordRow = 3;
+
+                                    int tempNo = 0;
+                                    for (int i = firstRecordRow; int.TryParse(ws.Cells[i, 1].Text.Trim(), out tempNo); i++)
+                                    {
+                                        var studentCode = ws.Cells[i, studentCodeCol].Text.Trim().ToUpper();
+                                        var studentInCourse = context.StudentInCourses.Where(q => q.Student.StudentCode.ToUpper().Equals(studentCode)).FirstOrDefault();
+
+                                        if (studentInCourse != null)
+                                        {
+                                            double average = 0;
+                                            for (var j = 5; j <= totalCol; j++)
+                                            {
+
+                                                double value = 0;
+                                                if (double.TryParse(ws.Cells[i, j].Text.Trim(), out value))
+                                                {
+
+                                                    StudentCourseMark studentCourseMark = null;
+
+                                                    var component = course.CourseMarks.Where(q => q.ComponentName.Contains(ws.Cells[titleRow, j].Text.Trim())).FirstOrDefault();
+
+                                                    studentCourseMark = context.StudentCourseMarks.
+                                                        Where(q => q.StudentInCourseId.Equals(studentInCourse.Id) && q.CourseMarkId.Equals(component.Id)).FirstOrDefault();
+
+                                                    var recordExisted = true;
+                                                    //remake null check
+                                                    if (studentCourseMark == null)
+                                                    {
+                                                        studentCourseMark = context.StudentCourseMarks.Create();
+                                                        recordExisted = false;
+                                                    }
+
+
+                                                    studentCourseMark.Mark = value;
+                                                    studentCourseMark.StudentInCourseId = studentInCourse.Id;
+                                                    if (component != null)
+                                                    {
+                                                        average += studentCourseMark.Mark.Value * component.Percentage / 100;
+                                                        studentCourseMark.CourseMarkId = component.Id;
+
+                                                        if(!recordExisted)
+                                                        {
+                                                            context.StudentCourseMarks.Add(studentCourseMark);
+                                                        }
+                                                    }
+                                                }
+                                            }
+
+                                            studentInCourse.Average = average;
+                                        }
+
+                                        context.SaveChanges();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No file has been uploaded" });
+                }
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json(new { success = false, message = e.Message });
+            }
+
+            return Json(new { success = true, message = "File uploaded successfully" });
         }
 
         public JsonResult GetSemesters()
@@ -152,6 +333,7 @@ namespace CaptstoneProject.Areas.Teacher.Controllers
 
     public class CourseDetailsViewModel
     {
+        public int CourseId { get; set; }
         public List<StudentInCourseViewModel> StudentInCourse { get; set; }
         public List<string> ComponentNames { get; set; }
         public string SubName { get; set; }
