08685e51845d761177ca3f92677e99300720b337
john <john@DANHDCSE61904>
2017-10-03 18:02:01 +0700

    +Add Enum

---
+++ CaptstoneProject/CaptstoneProject/Models/Enum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CaptstoneProject.Models
+{
+    public enum SememsterStatus
+    {
+        [Display(Name = "Open")]
+        Open = 0,
+        [Display(Name = "Pending")]
+        Pending = 1,
+        [Display(Name = "Closed")]
+        Closed = 2,
+    }
+
+    public enum StudentCourseStatus
+    {
+        [Display(Name = "Register")]
+        Register = 0,
+        [Display(Name = "Studying")]
+        Studying = 1,
+        [Display(Name = "Passed")]
+        Passed = 2,
+        [Display(Name = "Failed")]
+        Failed = 3,
+    }
+}
