8ef6e79b815a91778c7b0e34ce5fcc44c249fbee
StormNs <notstormns@gmail.com>
2017-10-05 17:39:36 +0700

    Add Role and normal Log in

---
+++ CaptstoneProject/CaptstoneProject/Areas/TrainingManagement/TrainingManagementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CaptstoneProject.Areas.TrainingManagement
+{
+    public class TrainingManagementAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TrainingManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TrainingManagement_default",
+                "TrainingManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- CaptstoneProject/CaptstoneProject/Models/AccountViewModels.cs
+++ CaptstoneProject/CaptstoneProject/Models/AccountViewModels.cs
@@ -66,6 +66,19 @@ namespace CaptstoneProject.Models
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
+    public class MyLoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+    }
+
 
     public class RegisterViewModel
     {
