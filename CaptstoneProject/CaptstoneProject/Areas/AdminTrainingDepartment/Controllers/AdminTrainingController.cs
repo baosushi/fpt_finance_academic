2b8bd63448f92c20012e70e2a5717f3d9a896952
StormNs <notstormns@gmail.com>
2017-10-29 01:18:02 +0700

    add report, export all student by semeter

--- CaptstoneProject/CaptstoneProject/Areas/AdminTrainingDepartment/Controllers/AdminTrainingController.cs
+++ CaptstoneProject/CaptstoneProject/Areas/AdminTrainingDepartment/Controllers/AdminTrainingController.cs
@@ -1,9 +1,11 @@
 ﻿using CaptstoneProject.Controllers;
 using CaptstoneProject.Models;
 using DataService.Model;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,21 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
         // GET: AdminTrainingDepartment/AdminTraining
         public ActionResult Index()
         {
+            using (var context = new DB_Finance_AcademicEntities())
+            {
+                var subjectGroupList = context.SubjectGroups.Select(q => new SelectListItem
+                {
+                    Text = q.Name,
+                    Value = q.Id.ToString()
+                }).ToList();
+                var semesterList = context.Semesters.Select(q => new SelectListItem
+                {
+                    Text = q.Title + " " + q.Year,
+                    Value = q.Id.ToString()
+                }).ToList();
+                ViewBag.SubjectGroups = subjectGroupList;
+                ViewBag.SemesterList = semesterList;
+            }
             return View();
         }
 
@@ -115,9 +132,9 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
                             SubCode = course.Subject.SubjectCode,
                             SubName = course.Subject.SubjectName,
                             //IsEditable = course.Status != (int)CourseStatus.Submitted ? false : true
-                            IsPublish = course.Status == (int)CourseStatus.InProgress ?  (int)FinalEditStatus.SubmitComponent :
-                            course.Status == (int)CourseStatus.Submitted ? (int)FinalEditStatus.EditFinal : 
-                            course.Status == (int)CourseStatus.FirstPublish ? (int)FinalEditStatus.EditRetake : 
+                            IsPublish = course.Status == (int)CourseStatus.InProgress ? (int)FinalEditStatus.SubmitComponent :
+                            course.Status == (int)CourseStatus.Submitted ? (int)FinalEditStatus.EditFinal :
+                            course.Status == (int)CourseStatus.FirstPublish ? (int)FinalEditStatus.EditRetake :
                             (int)FinalEditStatus.NoEdit,
                             StatusName = Enum.GetName(typeof(CourseStatus), course.Status == null ? 0 : course.Status.Value),
                         };
@@ -333,7 +350,7 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
         }
 
         [HttpPost]
-        public ActionResult UnlockStudent(string studentCode ,int courseId = -1)
+        public ActionResult UnlockStudent(string studentCode, int courseId = -1)
         {
             if (courseId == -1 || studentCode == null || studentCode.Length == 0)
             {
@@ -354,7 +371,7 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
                 Console.WriteLine(e);
                 return Json(new { success = false, message = e });
             }
-            return RedirectToAction("CourseDetails", new { courseId = courseId});
+            return RedirectToAction("CourseDetails", new { courseId = courseId });
         }
 
         public ActionResult GetTestResultCurrentSemester(int semesterId = 3)
@@ -425,6 +442,176 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
         }
 
 
+        //get study statistic of every subject in subjectGroup
+        public ActionResult StudyStatisticBySubjectGroup(int subjectGroupId, int semesterId = -1)
+        {
+            try
+            {
+                using (var context = new DB_Finance_AcademicEntities())
+                {
+                    var semester = semesterId == -1 ? context.Semesters.OrderByDescending(q => q.Year).
+                        ThenByDescending(q => q.SemesterInYear).
+                        FirstOrDefault() : context.Semesters.Find(semesterId);
+
+
+                    List<int> currentSubjectIds = context.Courses.Where(q => q.SemesterId == semester.Id &&
+                    q.Subject.SubjectGroup.Id == subjectGroupId).Select(q => q.SubjectId).Distinct().ToList();
+
+
+
+                    //List<string> subjectNames = new List<string>();
+                    //List<int> totalStudents = new List<int>();
+                    //List<int> totalPassStudents = new List<int>();
+                    //List<double> passPercentages = new List<double>();
+                    //List<double> minMarks = new List<double>();
+                    //List<double> maxMarks = new List<double>();
+
+                    List<SubjectStudyStatistic> statisticList = new List<SubjectStudyStatistic>();
+
+                    for (int i = 0; i < currentSubjectIds.Count(); i++)
+                    {
+                        var subjectId = currentSubjectIds[i];
+
+                        //could use join table 
+                        var list = context.Courses.Where(q => q.SubjectId == subjectId && q.SemesterId == semester.Id).
+                            AsEnumerable().Select(q => new IConvertible[] {
+
+                           q.StudentInCourses.Count(),//total
+                           q.StudentInCourses.Where(a => a.Status  == (int)StudentInCourseStatus.Passed).Count(),// pass
+                           q.StudentInCourses.Max(a => a.Average).Value, //max
+                           q.StudentInCourses.Min(a => a.Average).Value //min
+
+                        }).ToList();
+
+
+                        var total = 0;
+                        var totalPassed = 0;
+                        double passPercent = 0;
+                        double minMark = 0;
+                        double maxMark = 0;
+                        foreach (var item in list)
+                        {
+                            total += (int)item[0]; //total
+                            totalPassed += (int)item[1]; //pass
+                            if ((int)item[2] > maxMark)
+                            {
+                                maxMark = (int)item[2];
+                            }
+                            if ((int)item[3] < minMark)
+                            {
+                                minMark = (int)item[3];
+                            }
+                        }
+                        passPercent = totalPassed / total * 100;
+                        var subjectName = context.Subjects.Where(q => q.Id == subjectId).Select(q => q.SubjectName).FirstOrDefault().ToString();
+                        //subjectNames.Add(subjectName);
+                        //totalStudents.Add(total);
+                        //totalPassStudents.Add(totalPassed);
+                        //maxMarks.Add(maxMark);
+                        //minMarks.Add(minMark);
+                        statisticList.Add(new SubjectStudyStatistic
+                        {
+                            SubjectName = subjectName,
+                            TotalStudent = total,
+                            TotalPassedStudent = totalPassed,
+                            PassPecentage = passPercent,
+                            MaxMark = maxMark,
+                            MinMark = minMark
+                        });
+                    }
+
+
+                    return Json(new
+                    {
+                        success = true,
+                        iTotalRecords = statisticList.Count(),
+                        iTotalDisplayRecords = statisticList.Count(),
+                        aaData = statisticList
+                    }, JsonRequestBehavior.AllowGet);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Json(new { succcess = false, message = e });
+            }
+        }
+
+
+        public FileResult ExportAllStudentMarkExcelbySemester(int? semesterId)
+        {
+            List<StudentMarkInSemester> result = null;
+            string fileName = "Export_Mark_";
+            var semesterName = "";
+            //string semesterName = null;
+            using (var context = new DB_Finance_AcademicEntities())
+            {
+                //semesterName = context.Semesters.Where(q => q.Id == semesterId).Select(q => q.Title).SingleOrDefault();
+                result = (from c in context.Courses
+                          join sc in context.StudentInCourses
+                          on c.Id equals sc.CourseId
+                          where c.Id == semesterId
+                          select new StudentMarkInSemester
+                          {
+                              RollNumber = sc.StudentMajor.StudentCode,
+                              SubjectCode = c.Subject.SubjectCode,
+                              ClassName = c.ClassName,
+                              AverageMark = sc.Average,
+                              Status = Enum.GetName(typeof(StudentInCourseStatus), sc.Status == null ? 0 : sc.Status.Value)
+                          }).OrderBy(q => q.RollNumber).ToList();
+
+                semesterName= context.Semesters.Where(q => q.Id == semesterId).Select(q => q.Title + q.Year).FirstOrDefault();
+                fileName += semesterName;
+            }
+            MemoryStream ms = new MemoryStream();
+            ExcelPackage excelPack;
+            using (excelPack = new ExcelPackage(ms))
+            {
+
+                ExcelWorksheet ws1 = excelPack.Workbook.Worksheets.Add("Sheet1");
+
+                using (var range = ws1.Cells[1, 1, 1, 6])
+                {
+                    range.Style.Font.Bold = true;
+                    range.Style.Font.Size = 11;
+                }
+                ws1.Cells[1, 1].Value = "SemesterName";
+                ws1.Cells[1, 2].Value = "RollNumber";
+                ws1.Cells[1, 3].Value = "SubjectCode";
+                ws1.Cells[1, 4].Value = "ClassName";
+                ws1.Cells[1, 5].Value = "AverageMark";
+                ws1.Cells[1, 6].Value = "Status";
+
+                for (int i = 0; i < result.Count; i++)
+                {
+                    ws1.Cells[i + 2, 1].Value = semesterName;
+                    ws1.Cells[i + 2, 2].Value = result[i].RollNumber;
+                    ws1.Cells[i + 2, 3].Value = result[i].SubjectCode;
+                    ws1.Cells[i + 2, 4].Value = result[i].ClassName;
+                    ws1.Cells[i + 2, 5].Value = result[i].AverageMark;
+                    ws1.Cells[i + 2, 6].Value = result[i].Status;
+                }
+
+
+                var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                excelPack.SaveAs(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                return File(ms, contentType, semesterName + ".xlsx");
+            }
+        }
+
+        public class StudentMarkInSemester
+        {
+            public string SubjectCode { get; set; }
+            public string ClassName { get; set; }
+            public double? AverageMark { get; set; }
+            public string Status { get; set; } // 0:Fail, 1:Success
+            public string RollNumber { get; set; } // studentRollNumber
+
+        }
+
+
 
         public class MyData
         {
@@ -432,6 +619,16 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
             public String ClassName { get; set; }
             public String SubjectName { get; set; }
         }
+        public class SubjectStudyStatistic
+        {
+            public string SubjectName { get; set; }
+            public int TotalStudent { get; set; }
+            public int TotalPassedStudent { get; set; }
+            public double PassPecentage { get; set; }
+            public double MaxMark { get; set; }
+            public double MinMark { get; set; }
+        }
+
 
         public class CourseTestResult
         {
