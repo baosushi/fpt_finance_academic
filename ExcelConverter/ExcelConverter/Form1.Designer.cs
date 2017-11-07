0679a36d0af5fa3cebef0af0c0ec9bf41650b7e9
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2017-11-08 01:56:11 +0700

    Converter code

---
+++ ExcelConverter/ExcelConverter/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ExcelConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(437, 161);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.ForeColor = System.Drawing.Color.LightGray;
+            this.txtSource.Location = new System.Drawing.Point(12, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(409, 20);
+            this.txtSource.TabIndex = 2;
+            this.txtSource.Text = "Source folder";
+            this.txtSource.Enter += new System.EventHandler(this.txtSource_Enter);
+            this.txtSource.Leave += new System.EventHandler(this.txtSource_Leave);
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(437, 10);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 3;
+            this.btnSource.Text = "Browse...";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(437, 85);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnDestination.TabIndex = 5;
+            this.btnDestination.Text = "Browse...";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.ForeColor = System.Drawing.Color.LightGray;
+            this.txtDestination.Location = new System.Drawing.Point(12, 87);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(409, 20);
+            this.txtDestination.TabIndex = 4;
+            this.txtDestination.Text = "Destination folder";
+            this.txtDestination.Enter += new System.EventHandler(this.txtDestination_Enter);
+            this.txtDestination.Leave += new System.EventHandler(this.txtDestination_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 196);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.TextBox txtDestination;
+    }
+}
+
---
+++ ExcelConverter/ExcelConverter/Form1.cs
@@ -0,0 +1,191 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSource_Enter(object sender, EventArgs e)
+        {
+            if (txtSource.Text == "Source folder")
+            {
+                txtSource.Text = "";
+                txtSource.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtDestination_Enter(object sender, EventArgs e)
+        {
+            if (txtDestination.Text == "Destination folder")
+            {
+                txtDestination.Text = "";
+                txtDestination.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtSource_Leave(object sender, EventArgs e)
+        {
+            if (txtSource.Text == "")
+            {
+                txtSource.Text = "Source folder";
+                txtSource.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtDestination_Leave(object sender, EventArgs e)
+        {
+            if (txtDestination.Text == "")
+            {
+                txtDestination.Text = "Destination folder";
+                txtDestination.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtSource.Text = fbd.SelectedPath;
+                    txtSource.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtDestination.Text = fbd.SelectedPath;
+                    txtDestination.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            var sourceFolder = new DirectoryInfo(txtSource.Text);
+            var destinationFolder = new DirectoryInfo(txtDestination.Text);
+
+            foreach (var file in sourceFolder.GetFiles())
+            {
+                var stream = file.OpenRead();
+
+                ISheet sheet = null;
+                if (file.Extension.Equals(".xls"))
+                {
+                    HSSFWorkbook hssfwb = new HSSFWorkbook(stream);
+                    sheet = hssfwb.GetSheetAt(0);
+                }
+                else if (file.Extension.Equals(".xlsx"))
+                {
+                    XSSFWorkbook xssfwb = new XSSFWorkbook(stream);
+                    sheet = xssfwb.GetSheetAt(0);
+                }
+
+                if (sheet != null)
+                {
+                    IEnumerator rows = sheet.GetRowEnumerator();
+                    int? dataRow = null;
+                    int dataCol = 0; //default sheet
+                    var titleRow = sheet.GetRow(0);
+
+                    bool isEOS = titleRow.Cells[0].ToString().Trim() == "No" ? true : false;
+                    List<dynamic> records = new List<dynamic>();
+                    for (int i = 1; i < sheet.PhysicalNumberOfRows; i++)
+                    {
+                        var row = sheet.GetRow(i);
+                        string loginName = "";
+                        double mark = -1;
+
+                        if (isEOS)
+                        {
+                            loginName = row.Cells[1].ToString().Trim();
+                            mark = row.Cells[7].NumericCellValue;
+                        }
+                        else
+                        {
+                            if(row.GetCell(4, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null && row.GetCell(9, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null)
+                            {
+                                loginName = row.GetCell(4, MissingCellPolicy.RETURN_NULL_AND_BLANK).ToString().Trim().Split(new char[] { '@' })[0];
+                                mark = double.Parse(row.GetCell(9, MissingCellPolicy.RETURN_NULL_AND_BLANK).ToString());
+                            }
+                        }
+
+                        if (loginName != "" && mark != -1)
+                        {
+                            records.Add(new { LoginName = loginName, Mark = mark });
+                        }
+                    }
+
+                    DirectoryInfo temp = new DirectoryInfo(@destinationFolder.FullName + "\\Converted");
+
+                    if(!temp.Exists)
+                    {
+                        DirectoryInfo temp2 = new DirectoryInfo(@destinationFolder.FullName);
+                        temp2.CreateSubdirectory("Converted");
+                    }
+
+                    using (FileStream destinationFileStream = new FileStream(@destinationFolder.FullName + "\\Converted\\" + file.Name, FileMode.Create, FileAccess.Write))
+                    {
+                        IWorkbook wb = new XSSFWorkbook();
+                        ISheet sh = wb.CreateSheet("Sheet1");
+                        ICreationHelper cH = wb.GetCreationHelper();
+
+                        if(records.Count > 0)
+                        {
+                            IRow row = sh.CreateRow(0);
+                            ICell cell = row.CreateCell(0);
+                            cell.SetCellValue(cH.CreateRichTextString("Lop"));
+                            cell = row.CreateCell(1);
+                            cell.SetCellValue(cH.CreateRichTextString("Login Name"));
+                            cell = row.CreateCell(2);
+                            cell.SetCellValue(cH.CreateRichTextString("Mark"));
+                            cell = row.CreateCell(3);
+                            cell.SetCellValue(cH.CreateRichTextString("Note"));
+                        }
+
+                        for (int i = 0; i < records.Count; i++)
+                        {
+                            IRow row = sh.CreateRow(i + 1);
+                            ICell cell = row.CreateCell(0);
+                            cell.SetCellValue(cH.CreateRichTextString(""));
+                            cell = row.CreateCell(1);
+                            cell.SetCellValue(cH.CreateRichTextString(records[i].LoginName));
+                            cell = row.CreateCell(2);
+                            cell.SetCellValue(records[i].Mark);
+                            cell = row.CreateCell(3);
+                            cell.SetCellValue(cH.CreateRichTextString(""));
+                        }
+
+                        wb.Write(destinationFileStream);
+                    }
+                }
+            }
+        }
+    }
+}
