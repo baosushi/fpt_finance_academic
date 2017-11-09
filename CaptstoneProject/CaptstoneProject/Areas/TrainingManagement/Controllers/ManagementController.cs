069c85ae3be2b2b2162b4da363211f151431e971
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2017-11-09 14:18:54 +0700

    update import data to real db and course selection

--- CaptstoneProject/CaptstoneProject/Areas/TrainingManagement/Controllers/ManagementController.cs
+++ CaptstoneProject/CaptstoneProject/Areas/TrainingManagement/Controllers/ManagementController.cs
@@ -922,6 +922,7 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
             return Json(new { success = true, message = "Successully submitted!" });
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public ActionResult UploadSubject()
         {
@@ -934,7 +935,7 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
                 }
                 foreach (string file in Request.Files)
                 {
-                    HttpPostedFileBase fileContent = Request.Files[file];
+                    var fileContent = Request.Files[file];
                     if (fileContent != null && fileContent.ContentLength > 0)
                     {
                         string[] segments = fileContent.FileName.Split('.');
@@ -984,15 +985,17 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
                                         {
                                             var subCode = ws.Cells[i, 2].Text.Trim();
                                             var subName = ws.Cells[i, 4].Text.Trim();
+                                            var subjectCredit = -1;
+                                            int.TryParse(ws.Cells[i, 5].Text.Trim(), out subjectCredit);
                                             var existList = context.Subjects.Where(q => q.SubjectCode.Equals(subCode)).ToList();
                                             if (existList.Count == 0)
-                                                context.Subjects.Add(new Subject { SubjectCode = subCode, SubjectName = subName });
+                                                context.Subjects.Add(new Subject { SubjectCode = subCode, SubjectName = subName, CreditValue = subjectCredit });
 
+                                            context.SaveChanges();
                                         }
                                     }
 
                                 }
-                                context.SaveChanges();
                             }
                         }
                         stream.Close();
@@ -1026,7 +1029,8 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
             return Json(new { success = true, message = "Upload Students successed" });
         }
 
-        public ActionResult ImportSubject()
+        [AllowAnonymous]
+        public ActionResult SubjectManagement()
         {
             return View();
         }
@@ -1209,7 +1213,8 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
             {
                 var block = context.Semesters.OrderBy(q => q.Year).ThenBy(q => q.SemesterInYear).LastOrDefault().Blocks.Where(q => q.Status == (int)BlockStatus.Registering).FirstOrDefault();
 
-                var availableSubjects = block.AvailableSubjects.GroupBy(q => q.SubjectId).Select(q => new {
+                var availableSubjects = block.AvailableSubjects.GroupBy(q => q.SubjectId).Select(q => new
+                {
                     Count = q.Count(),
                     SubjectId = q.Key
                 });
@@ -1221,7 +1226,7 @@ namespace CaptstoneProject.Areas.TrainingManagement.Controllers
                     if (subject != null)
                     {
                         var registrationCount = registeredSubject.Count;
-                        
+
                     }
                     else
                     {
