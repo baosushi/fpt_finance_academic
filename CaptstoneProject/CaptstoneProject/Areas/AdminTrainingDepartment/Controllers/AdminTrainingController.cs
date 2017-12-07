ed4ef058f2351b1c416124f81a0c2d5778ea008f
john <john@DANHDCSE61904>
2017-12-07 20:26:16 +0700

    Fix some UI and some bugs

--- CaptstoneProject/CaptstoneProject/Areas/AdminTrainingDepartment/Controllers/AdminTrainingController.cs
+++ CaptstoneProject/CaptstoneProject/Areas/AdminTrainingDepartment/Controllers/AdminTrainingController.cs
@@ -38,7 +38,7 @@ namespace CaptstoneProject.Areas.AdminTrainingDepartment.Controllers
             return View();
         }
 
-        public ActionResult SemesterManagement(int? semesterId)
+        public ActionResult SemesterManagement(int semesterId=-1)
         {
             try
             {
